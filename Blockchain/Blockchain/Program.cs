7e23a3c669ffce0ff61b1602627b39549a2977a5
diegobaqt <dabaquerot@hotmail.com>
2018-05-22 09:33:02 -0500

    Feat: Add Blockchain example in C#

---
+++ Blockchain/Blockchain/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NBitcoin;
+
+namespace Blockchain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create private Key
+            var privateKey = new Key();
+
+            // Genera la llave pública a partir de la llave privada
+            var publicKey = privateKey.PubKey;
+            Console.WriteLine("Public Key: " + publicKey);
+
+            var publicKeyHash = publicKey.Hash;
+            Console.WriteLine("Public Key Hash: " + publicKeyHash);
+
+            // Red de Bitcoin que todos utilizan.
+            var mainNetAddress = publicKeyHash.GetAddress(Network.Main);
+            Console.WriteLine("Address (Bitcoin Network): " + mainNetAddress);
+
+            // Red de Bitcoin para propósito de desarrollo. Los Bitcoins en esta red no valen nada.
+            var testNetAddress = publicKeyHash.GetAddress(Network.TestNet);
+            Console.WriteLine("Address (Bitcoin Network for Development): " + testNetAddress);
+
+            // Es un pequeño script que explica qué condiciones se deben cumplir para reclamar 
+            // la propiedad de bitcoins.
+            Console.WriteLine("ScriptPubKey (BN): " + mainNetAddress.ScriptPubKey); 
+            Console.WriteLine("ScriptPubKey (BND): " + testNetAddress.ScriptPubKey);
+
+            // Genera nuestro Bitcoin secret (también conocido como Wallet Import Format o simplemente WIF) 
+            // desde nuestra llave privada para la red principal.
+            var mainNetPrivateKey = privateKey.GetBitcoinSecret(Network.Main);
+            Console.WriteLine("WIF (Main Network): " + mainNetPrivateKey);
+            
+            var transaction = new Transaction();
+            transaction.GetTransaction();
+        }
+    }
+}
---
+++ Blockchain/Blockchain/Transaction.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using NBitcoin;
+using QBitNinja.Client;
+using QBitNinja.Client.Models;
+
+namespace Blockchain
+{
+    public class Transaction
+    {
+        private static QBitNinjaClient _client;
+        private static uint256 _transactionId;
+        public Transaction()
+        {
+            var client = new QBitNinjaClient(Network.Main);
+
+            // Parse transaction id to NBitcoin.uint256 so the client can eat it
+            var transactionId = uint256.Parse("f13dc48fb035bbf0a6e989a26b3ecb57b84f85e0836e777d6edf60d87a4a2d94");
+
+            _client = client;
+            _transactionId = transactionId;
+        }
+
+        public void GetTransaction()
+        {
+            // Query the transaction
+            var transactionResponse = _client.GetTransaction(_transactionId).Result;
+
+            var transaction = transactionResponse.Transaction;
+
+            Console.WriteLine("TransactionId: " + transactionResponse.TransactionId); 
+            Console.WriteLine("Transaction Hash: " + transaction.GetHash());
+
+            var receivedCoins = transactionResponse.ReceivedCoins;
+            foreach (var coin in receivedCoins)
+            {
+                var amount = (Money) coin.Amount;
+
+                Console.WriteLine("Amount: " + amount.ToDecimal(MoneyUnit.BTC));
+                var paymentScript = coin.TxOut.ScriptPubKey;
+                Console.WriteLine("Payment Script: " + paymentScript);  // It's the ScriptPubKey
+                var address = paymentScript.GetDestinationAddress(Network.Main);
+                Console.WriteLine("Address: " + address); // 1HfbwN6Lvma9eDsv7mdwp529tgiyfNr7jc
+            }
+        }
+    }
+}
